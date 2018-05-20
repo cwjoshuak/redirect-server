63e58de1f2bfa4de2a47bb206bb82e35d29bb96b
cwjoshuak <jkuan1997@gmail.com>
2018-05-20 10:58:41 -0700

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,15 +1,10 @@
 import Vapor
-
+import HTTP
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("/") { req in
+        return req.redirect(to: "http://54.183.243.96", type: .permanent)
     }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
